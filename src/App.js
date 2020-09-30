2074e0e363395c310d8bbd7c58e0c2ab9436c7fd
osamudiamen <osas.azamegbe@gmail.com>
2020-10-01 00:25:36 +0100

    added basic styling, Form and TodoList components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
-      <h1>Todo</h1>
+      <header>
+        <h1>TODO</h1>
+      </header>
+      <Form/>
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <div>
+            <form>
+                <input className="todo-input" type="text" placeholder="add an item to your Todo List" />
+                <button className="todo-button" type="submit" >
+                    <i className=""></i>
+                </button>
+                <div className="select">
+                    <select name="todos" className="filter-todos" >
+                        <option value="all" >All</option>
+                        <option value="completed" >Completed</option>
+                        <option value="uncompleted" >Uncompleted</option>
+                    </select>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
